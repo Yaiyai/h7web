7a0571c7a0ee311f8db6af04796357a8c31adc4d
yaiyai <yaiza.delrio@clupik.com>
2023-10-28 17:46:57 +0200

    quote section

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -6,6 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./quote.component.scss']
 })
 export class QuoteComponent {
-  @Input() quote: string = 'soy una quote feisima y larguisima que no aporta nada';
-  @Input() bkg: string = 'https://res.cloudinary.com/h7metrology/image/upload/v1698506217/web/bkg.jpg';
+  @Input() quote: any;
 }
