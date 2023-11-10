04616e7b1743bc7ae081b71e0e6b1c33d618b1c0
yaiyai <yaiza.delrio@clupik.com>
2023-11-10 11:47:06 +0100

    subvenciones finish

--- src/app/pages/subvenciones/subvenciones.component.ts
+++ src/app/pages/subvenciones/subvenciones.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { ApiSectionsService } from 'src/app/communication/api-sections.service';
 
 @Component({
   selector: 'app-subvenciones',
@@ -7,9 +8,31 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./subvenciones.component.scss']
 })
 export class SubvencionesComponent implements OnInit {
+  public sub: any;
+  public get bkg() {
+    return this.sub?.uniqueImage;
+  }
+
+  public get title() {
+    return this.sub?.title || '';
+  }
+
+  public get text() {
+    return this.sub?.text1 || '';
+  }
+  public get text2() {
+    return this.sub?.text2 || '';
+  }
+  public get images() {
+    return this.sub?.gallery || [];
+  }
+  public get subimages() {
+    return this.sub?.sub || [];
+  }
 
   constructor(
     private translate: TranslateService,
+    private apiSectionsService: ApiSectionsService,
   ) {
     this.translate.onLangChange.subscribe(res => {
       this.getData();
@@ -17,10 +40,20 @@ export class SubvencionesComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // this.getData();
   }
 
   getData() {
-
+    this.apiSectionsService.getSection(this.translate.currentLang === 'es' ? '654e03ffbdcbb100194f8f88' : '654e03ffbdcbb100194f8f88').subscribe({
+      next: section => {
+        this.sub = section.section;
+        let aux = section.section.gallery.filter((elm: any) => elm);
+        this.sub.text1 = section.section.text.split('((()))')[0];
+        this.sub.text2 = section.section.text.split('((()))')[1];
+        this.sub.gallery = [...aux].slice(0, -3);
+        this.sub.sub = [...aux].slice(1).slice(-3);
+        console.log(this.sub);
+      }
+    });
   }
-
 }
